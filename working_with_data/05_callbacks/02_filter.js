8f9c3a62f33faca5b7d504e64413bc74a644bcf1
alicezehner <alicezehner88@gmail.com>
2022-02-09 10:39:19 -0500

    end of class

--- working_with_data/05_callbacks/02_filter.js
+++ working_with_data/05_callbacks/02_filter.js
@@ -1,7 +1,11 @@
 var list = [5,8,9,2,7,6,3,1,4];
 
-function callback(item){
-    // return true for included items
-}
 
-var filtered = list.filter(callback);
+// could define the function seperately but can also define a function within the parameter
+// .filter returns elements in list calledback as true
+var filtered = list.filter(function callback(item){
+    // return true for included items
+    return item >5;
+});
+
+console.log(filtered);
